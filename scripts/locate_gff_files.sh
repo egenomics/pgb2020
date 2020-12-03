b50a5b2fa4acadbbe46c91a38c1c976bb7c2134d
egenomics <jlvillanueva@clinic.cat>
2020-12-03 12:58:49 +0100

    update analysis

--- scripts/locate_gff_files.sh
+++ scripts/locate_gff_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-main_dir=/home/jl/Dropbox/university/UPF_msc/PGB_2020/project_summary
+main_dir=/home/jl/Dropbox/university/UPF_msc/PGB_2020/project_summary/pgb2020
 mkdir $main_dir/results
 
 #Create basic statistics tab separated empty file (only header)
@@ -34,4 +34,19 @@ do
     echo -e $genus'\t'$species'\t'$dataset'\t'$num_transcripts'\t'$total_length'\t'$num_chr'\t'$onlyfile
     echo -e $genus'\t'$species'\t'$dataset'\t'$num_transcripts'\t'$total_length'\t'$num_chr'\t'$onlyfile >> $main_dir/results/basic_statistics.tsv
   done
+done < "$input"
+
+
+#Obtain length of all fasta files (You have to download all species data from drive)
+input=${main_dir}/metadata/species_sorted.txt
+fasta_location=/media/jl/DATA_Gianca/pgb_project_2020
+
+while IFS= read -r line
+do
+  binomial=(${line// / }) #Use space as separator to get information in an array
+  genus=`echo ${binomial[0]::1} | sed -e 's/\(.*\)/\L\1/'` #Position one is genus, then lower case
+  species=${binomial[1]} #Position two is species
+  file=`find $fasta_location'/'${genus}'_'${species}'/' -iname "*$species.fa" -type f`
+  output_f=${main_dir}/results/${species}_fasta_lengths.tsv
+  python $main_dir/scripts/seq_len.py $file > $output_f
 done < "$input"
